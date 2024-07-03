29b8f9f93e021b08617249ebb3fc24493b7a1583
softengineerstevens <softengineerstevens@gmail.com>
2024-07-03 18:12:34 +0300

    DOM manipulation

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+
+const element = document.querySelector("#main");
+element.remove()
+console.log(element);
+
+
+let newHeader = document.createElement('h1');
+
+newHeader.setAttribute("id", "victory");
+
+newHeader.textContent = "Stephen Ombwodo is the champion"
